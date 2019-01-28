3a1d0a8320c8e8530f2aca59391ec3f396fd884e
Dat Nguyen <Dat_T_Nguyen@live.com>
2019-01-28 14:45:30 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+AOS.init();
+
+var rellax = new Rellax(".rellax");
+
+function openNav() {
+  if ($(window).width() <= 1024) {
+    document.getElementById("nav").style.height = "90%";
+  } else {
+    document.getElementById("nav").style.height = "75%";
+  }
+}
+
+function closeNav() {
+  document.getElementById("nav").style.height = "0%";
+}
+
+$(".box, .container").on("click", function() {
+  if (parseInt($("#nav").css("height")) > 0) {
+    closeNav();
+  }
+});
+
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $(".scroll, .scroll-line").addClass("hidden");
+  } else {
+    $(".scroll, .scroll-line").removeClass("hidden");
+  }
+});
+
+$(document).ready(function() {
+  $(".bg").hover(function() {
+    $("#projects").addClass("show");
+  });
+});
+
+$("#container").on("click", function() {
+  if (parseInt($("#nav").css("height")) > 0) {
+    closeNav();
+  }
+});
+
+$(".footerimage a").on("click", function(e) {
+  e.preventDefault();
+  var goTo = this.getAttribute("href");
+  $("html").fadeOut(500);
+
+  setTimeout(function() {
+    window.location = goTo;
+  }, 500);
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
